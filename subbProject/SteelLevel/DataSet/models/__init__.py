efd9866e27371aa460952cba34938d9f810bdeb7
lcx <1042815796@qq.com>
2024-06-01 15:28:51 +0800

    add server Config

--- subbProject/SteelLevel/DataSet/models/__init__.py
+++ subbProject/SteelLevel/DataSet/models/__init__.py
@@ -8,6 +8,8 @@ class SteelLevel(Base):
     #  钢板等级信息表
     __tablename__ = 'SteelLevel'
     id = Column(Integer, primary_key=True)
+    plant_classification = Column(String)
+    productionLine_classification = Column(String)
     steelName = Column(String)
     packageName = Column(String)
     seqNo = Column(Integer)
@@ -23,6 +25,16 @@ class SteelLevel(Base):
     levelInfo = Column(String)
     grade = Column(Integer)
     msg = Column(String)
+
+    level_up = Column(Integer)
+    levelInfo_up = Column(String)
+    grade_up = Column(Integer)
+    msg_up = Column(String)
+
+    level_under = Column(Integer)
+    levelInfo_under = Column(String)
+    grade_under = Column(Integer)
+    msg_under = Column(String)
 
 
 class DefectLevel(Base):
@@ -54,4 +66,20 @@ class DefectLevel(Base):
     levelMsg = Column(String)
     msg = Column(String)
 
+
 class UserData(Base):
+    __tablename__ = 'UserSteelLevelData'
+    id = Column(Integer, primary_key=True, autoincrement=True)  # id
+    fileName = Column(String)  # 插入的文件名称
+    steelName = Column(String)
+    plant_classification = Column(String)
+    productionLine_classification = Column(String)
+    productionTime = Column(DateTime)
+    steelType = Column(String)
+    steelLevel = Column(Integer)
+    steelLevelCode = Column(String)
+    residualReasonCode = Column(String)
+    defectCode = Column(String)
+    scrapReasonCode = Column(String)
+    downgradeNote = Column(String)
+    BusNumber = Column(String)
